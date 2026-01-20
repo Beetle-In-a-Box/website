6e5d978c6c8e2537ef071c693e3f5f3acf84bf47
michaelslain <misha.slain@icloud.com>
2026-01-19 19:36:05 -0800

    moved ocf banner

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -6,25 +6,27 @@ export default function Footer() {
   const year = getCurrentYear();
   return (
     <footer className={styles.footer}>
-      <div>
-        <p>Copyright © {year} Beetle in a Box / Leo Abubucker | All Rights Reserved</p>
-        <p>Website Development By Leo Abubucker</p>
-        <p>
-          Disclaimer:
-          <i>
-            {' '}We are a student group acting independently of the University of California. We take full
-            responsibility for our organization and this web site.
-          </i>
-        </p>
+      <div className={styles.footerContent}>
+        <div>
+          <p>Copyright © {year} Beetle in a Box / Leo Abubucker | All Rights Reserved</p>
+          <p>Website Development By Leo Abubucker</p>
+          <p>
+            Disclaimer:
+            <i>
+              {' '}We are a student group acting independently of the University of California. We take full
+              responsibility for our organization and this web site.
+            </i>
+          </p>
+        </div>
+        <a href="https://www.ocf.berkeley.edu" target="_blank" rel="noopener noreferrer" className={styles.ocfLink}>
+          <Image
+            src="http://www.ocf.berkeley.edu/hosting-logos/ocf-hosted-penguin.svg"
+            alt="Hosted by the OCF"
+            width={88}
+            height={31}
+          />
+        </a>
       </div>
-      <a href="https://www.ocf.berkeley.edu" target="_blank" rel="noopener noreferrer">
-        <Image
-          src="http://www.ocf.berkeley.edu/hosting-logos/ocf-hosted-penguin.svg"
-          alt="Hosted by the OCF"
-          width={88}
-          height={31}
-        />
-      </a>
     </footer>
   );
 }
